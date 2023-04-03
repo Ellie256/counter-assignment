f181e488df90cdceb084edbdca241f5c1b9d3a99
Ellie256 <61980311+Ellie256@users.noreply.github.com>
2023-04-03 13:32:00 -0400

    Added Unit Tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,34 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
+	render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+	const counterMessage = screen.getByText(/Counter/i);
+	expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+	const initCount = screen.getByTestId("count");
+	expect(initCount.textContent).toBe("0");
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+	const plusBtn = screen.getByRole("button", {name: "+"});
+	userEvent.click(plusBtn);
+
+	const plusCount = screen.getByTestId("count");
+	expect(plusCount.textContent).toBe("1");
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+	const minusBtn = screen.getByRole("button", {name: "-"});
+	userEvent.click(minusBtn);
+
+	const minusCount = screen.getByTestId("count");
+	expect(minusCount.textContent).toBe("-1");
 });
